464b5bc082aae31f5b9331dd48b6bc8bdfe7397c
iromli <isman.firmansyah@gmail.com>
2021-01-26 23:08:26 +0700

    tls-secrets.sh: always import cert and key

--- nginx/tls-secrets.sh
+++ nginx/tls-secrets.sh
@@ -1,23 +1,19 @@
 #!/bin/sh
 
-if [ ! -f dhparam.pem ]; then
-    openssl dhparam -out dhparam.pem 2048
-fi
+# if [ ! -f dhparam.pem ]; then
+#     openssl dhparam -out dhparam.pem 2048
+# fi
 
-kubectl -n gluu create secret generic tls-dhparam --from-file=dhparam.pem
+# kubectl -n gluu create secret generic tls-dhparam --from-file=dhparam.pem
 
-if [ ! -f ingress.crt ]; then
-    kubectl -n gluu get secret gluu -o json \
+kubectl -n gluu get secret gluu -o json \
     | grep '"ssl_cert":' \
     | awk -F '"' '{print $4}' \
     | base64 --decode > ingress.crt
-fi
 
-if [ ! -f ingress.key ]; then
-    kubectl -n gluu get secret gluu -o json \
+kubectl -n gluu get secret gluu -o json \
     | grep '"ssl_key":' \
     | awk -F '"' '{print $4}' \
     | base64 --decode > ingress.key
-fi
 
 kubectl -n gluu create secret tls tls-certificate --key ingress.key --cert ingress.crt
